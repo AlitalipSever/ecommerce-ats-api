3e55f650eaff04dd320ea9d80866b47baa909347
AlitalipSever <alitalipsever@gmail.com>
2022-02-01 20:31:57 +0100

    cart and order models added

--- models/Order.js
+++ models/Order.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose")
 
-const CartSchema = new mongoose.Schema({
-        title: {type: String, required:true, unique:true},
-        desc: {type: String, required:true},
-        img:{type: String, required:true},
-        categories:{type: Array },
-        size:{type: String},
-        color:{type: String},
-        price:{type: Number, required:true},
-
+const OrderSchema = new mongoose.Schema({
+            userId: {type: String, required:true},
+            products: [
+                    {
+                            productId:{type: String},
+                            quantity:{type: Number, default:1}
+                    }
+            ],
+            amount: {type: Number, required: true},
+            address: {type: Object, required: true},
+            status: {type: String, default: "pending"},
 
     },
     {timestamps:true},
 
 );
 
-module.exports = mongoose.model("Product", CartSchema)
+module.exports = mongoose.model("Order", OrderSchema)
