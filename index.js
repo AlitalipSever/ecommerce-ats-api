29faf01642c44ba21ac95fc29a0ac4a4d5b427d8
AlitalipSever <alitalipsever@gmail.com>
2022-02-03 09:30:18 +0100

    auth added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL)
@@ -11,6 +14,10 @@ mongoose.connect(process.env.MONGO_URL)
     }).catch((error)=>{
     console.log(error);
     })
+
+app.use(express.json())
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(process.env.PORT || 5001, ()=>{
     console.log(`Server is running at 5001`);
