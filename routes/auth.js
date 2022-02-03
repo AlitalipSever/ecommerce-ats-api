29b7afe541e06c3f2c332945b32b8d739d9d494b
AlitalipSever <alitalipsever@gmail.com>
2022-02-03 11:30:26 +0100

    user route added

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,7 @@ router.post("/login", async (req, res)=>{
         originalpassword !== req.body.password && res.status(401).json("wrong credentials!!!!")
 
         const accessToken = jwt.sign({id:user._id, isAdmin:user.isAdmin},process.env.JWT_SEC,{expiresIn: "3d"})
-
+        // send user_id and isAdmin as JWT
         const {password, ...others} = user._doc
         res.status(200).json({...others, accessToken})
     }catch (err) {
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,44 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const CryptoJS = require("crypto-js")
+const {verifToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken")
 
-//Register
-router.post("/register", async  (req,res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email:req.body.email,
-        password:req.body.password
-    })
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password,process.env.PASS_SEC).toString()
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true});                              // send new user
+
+        res.status(500).json(updatedUser)
+    }catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+     try{
+         await User.findByIdAndDelete(req.params.id)
+         res.status(200).json("user has been deleted")
+     }catch (e) {
+         res.status(500).json(e)
+     }
+})
+
+
+// GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res)=>{
     try{
-        const savedUser = await newUser.save()
-        res.status(201)             // successful
-    }catch (err){
-        console.log(err);
-        res.status(500).json(err)
+        const user = await User.findById(req.params.id)
+        const {password, ...others} = user._doc
+        res.status(200).json(others)
+    }catch (e) {
+        res.status(500).json(e)
     }
 })
 
